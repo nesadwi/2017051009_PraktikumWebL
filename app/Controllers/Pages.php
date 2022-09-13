80ceb1cd874f1419ccbcd1b321cbb81bb120c181
nesadwi <nesadwi231@gmail.com>
2022-09-13 12:38:55 +0700

    praktikum3

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -6,7 +6,7 @@ class Pages extends BaseController
 {
     public function view($page = 'home')
     {
-        if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+        if (!is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
             // Whoops, we don't have a page for that!
             throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
         }
